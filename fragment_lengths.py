317efe0f29cd837e06187beeb6a0a7d286adb38b
ospupegam <ospupegam@yahoo.com>
2019-03-01 08:52:40 +0200

    course project # 3 : fragment_lengths

--- fragment_lengths.py
+++ fragment_lengths.py
@@ -1,3 +1,11 @@
+"""
+Restriction fragment lengths
+Create a python script in your course project directory called fragment_lengths.py
+This script should  calculate the size of the two fragments that will be produced when the DNA sequence is digested with EcoRI
+ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT
+    Hint:
+The sequence contains a recognition site for the EcoRI restriction enzyme, which cuts at the motif G*AATTC (the position of the cut is indicated by an asterisk)
+"""
 dna="ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
 EcoRI="GAATTC"
 EcoRI_index=dna.find(EcoRI)
