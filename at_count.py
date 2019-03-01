2aaf04c190edc0edc908f802b44d3d76332d8621
ospupegam <ospupegam@yahoo.com>
2019-03-01 08:10:14 +0200

    course project # 1 : at_count

--- at_count.py
+++ at_count.py
@@ -1,3 +1,10 @@
+"""
+Create a python script in your course project directory called ae_content.py
+This script should print the AT content of this short DNA sequence
+ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT
+    Hint:
+AT content of a sequence is the number of A & T bases divided by the total number of bases.
+"""
 dna="ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 a=dna.upper().count("A")
 t=dna.upper().count("T")
